d63a5af16fb792506db2d63d019f2f176538fbe4
NWiktor <31969508+NWiktor@users.noreply.github.com>
2023-03-28 22:12:43 +0200

    rearrange code for readability

--- logger.py
+++ logger.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
-""" Logger modul for other programs/apps. This modul contains all settings
-for the logger, which saves the logs of the last five run.
+""" Logger modul for other programs/apps.
+
+Initializes and returs a logger named *'Main'* for all modules. This modul
+contains all settings for the logger. The logger level is DEBUG, as it is
+intended to catch every log message. **EVERY MODUL WITHIN THIS PROJECT SCOPE
+MUST USE THE FOLLOWING SYNTAX AT THE HEADER TO ACCESS THIS LOGGER:**
+
+.. code-block::
+
+   from logger import MAIN_LOGGER as l
+
+The logger consists of two log handler: a console handler, and filehandler,
+the latter uses filerotating and stores the logs of the last five (5)
+running.
 
 Help
 ----
@@ -22,46 +34,7 @@ import sys
 from logging.handlers import RotatingFileHandler
 
 
-def init_logger():
-    """ Initializes a logger named *'Main'* for all modules.
-
-    This function is called when this module is first imported. When this
-    happens, it creates a logger, which is intended to used by every other
-    modules. The logger level is DEBUG, as it is intended to catch every
-    log message. **EVERY MODUL WITHIN THIS PROJECT SCOPE MUST USE THE FOLLOWING
-    SYNTAX AT THE HEADER TO ACCESS THIS LOGGER:**
-
-    .. code-block::
-
-       from logger import MAIN_LOGGER as l
-
-    The logger consists of two log handler: a console handler, and filehandler,
-    the latter uses filerotating and stores the logs of the last five (5)
-    running.
-    """
-
-    global MAIN_LOGGER
-
-    MAIN_LOGGER = logging.getLogger("Main")
-    MAIN_LOGGER.setLevel(logging.DEBUG)
-
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(FORMATTER)
-    MAIN_LOGGER.addHandler(console_handler)
-
-    # Nincs felső byte limit - max. 5 indítás logját tartalmazza
-    file_handler = RotatingFileHandler(LOG_FILE, maxBytes=0, backupCount=5)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(FORMATTER)
-    file_handler.doRollover() # Átforgatja a logot minden indításnál
-    MAIN_LOGGER.addHandler(file_handler) # Format, és file handler beállítások
-    MAIN_LOGGER.info("Main logger created!")
-
-
-## Running script
-
-# Ha ez nincs, akkor .exe file nem működik!
+# Wihtout this, the .exe file not works!
 if getattr(sys, 'frozen', False):
     initdir = os.path.dirname(sys.executable)
 elif __file__:
@@ -76,12 +49,26 @@ FORMATTER = logging.Formatter(
 '%(asctime)s %(module)s [%(levelname)s] : %(message)s',
 datefmt='%Y/%m/%d %H:%M:%S')
 
-# Ha a naplófájl mappa hiányzik, létrehozom
+# If logging folder is missing, create it
 if not os.path.isdir(os.path.join(initdir, LOG_DIRPATH)):
     os.mkdir(os.path.join(initdir, LOG_DIRPATH))
 
 # Initializes logger for all the modules
-init_logger()
+MAIN_LOGGER = logging.getLogger("Main")
+MAIN_LOGGER.setLevel(logging.DEBUG)
+# Console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(FORMATTER)
+MAIN_LOGGER.addHandler(console_handler)
+
+# File handler, no upper limit, max. 5 run is logged
+file_handler = RotatingFileHandler(LOG_FILE, maxBytes=0, backupCount=5)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(FORMATTER)
+file_handler.doRollover() # Rolls log at each start
+MAIN_LOGGER.addHandler(file_handler)
+MAIN_LOGGER.info("Main logger created!")
 
 
 ## Modul-teszt
